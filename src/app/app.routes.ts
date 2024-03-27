22af7413ad31462e3cc0c7317149b1fae80e6cb6
harichandanavitasoft <harichandanavita@gmail.com>
2024-03-27 09:41:46 +0530

    about design completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { HeaderComponent } from './header/header.component';
 
 export const routes: Routes = [
-    {path:'',component:HomeComponent},
+    {path:'',component:HeaderComponent,children:[
+        {path:'',component:HomeComponent},
+
+    
     {path:'about',component:AboutComponent}
+    ]}
 ];
