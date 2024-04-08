58938d03372daf535c7691e25c5f91c9fac13c90
harichandanavitasoft <harichandanavita@gmail.com>
2024-04-08 14:52:48 +0530

    about design completed

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
@@ -10,5 +10,16 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  isScrolled: boolean = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    // logic to check scroll position and toggle isScrolled
+    if (window.scrollY > 100) { // example threshold for changing color
+      this.isScrolled = true;
+    } else {
+      this.isScrolled = false;
+    }
+  }
 
 }
