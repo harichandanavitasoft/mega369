1670d7795cc4d959100d08f7aeb8a6eb1c8dd21e
harichandanavitasoft <harichandanavita@gmail.com>
2024-04-08 15:29:59 +0530

    blog design completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { HeaderComponent } from './header/header.component';
+import { BlogComponent } from './blog/blog.component';
 
 export const routes: Routes = [
     {path:'',component:HeaderComponent,children:[
         {path:'',component:HomeComponent},
-
-    
-    {path:'about',component:AboutComponent}
+         {path:'about',component:AboutComponent},
+         {path:'blog',component:BlogComponent}
     ]}
 ];
--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-blog',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,RouterLink,RouterOutlet],
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.css'
 })
