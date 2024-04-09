0e30d416f7071a97956a9b5a9efce8e850672116
harichandanavitasoft <harichandanavita@gmail.com>
2024-04-09 16:05:52 +0530

    contact design completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { HeaderComponent } from './header/header.component';
 import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
     {path:'',component:HeaderComponent,children:[
         {path:'',component:HomeComponent},
          {path:'about',component:AboutComponent},
-         {path:'blog',component:BlogComponent}
+         {path:'blog',component:BlogComponent},
+         {path:'contact',component:ContactComponent}
     ]}
 ];
